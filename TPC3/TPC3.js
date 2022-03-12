012460d4fa3b32faa842b88aa590fad02e056587
haz145 <saralexandramarques@gmail.com>
2022-03-12 02:42:21 +0000

    it works now (wip)

--- TPC3/TPC3.js
+++ TPC3/TPC3.js
@@ -4,70 +4,104 @@ TPC3
 - api dados: json-server na porta 3000
 - serviço na porta 4000 que responde:
     - localhost:4000/  -> página web com 3 links (lista de alunos, de cursos e de instrumentos)
-                          <a href="localhost:4000/alunos">lista de alunos</a>
+                          <a href="http://localhost:4000/alunos">lista de alunos</a>
                           -> responde com uma página web com uma tabela com id, nome, curso, e instrumento dos alunos
 
 npm install -g json-server
 npm install axios --save
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 json-server --watch db.json --port 3000
 */
 
 var http = require('http')
 var url = require('url')
-//const axios = require('axios');
-var json = require('./db.json')
+const axios = require('axios');
 
-function generatePage(type){
-    var html = '<!DOCTYPE html><html><head><title>TPC2</title><meta charset="UTF-8"/>' +
+var basehtml = '<!DOCTYPE html><html><head><title>TPC3</title><meta charset="UTF-8"/>' +
             '<style>body {margin: 40px;font-family:\'Courier New\';}</style></head><body>'
 
-    switch(type){
-        case 0: //error
-            html += '<h1><b>Page Not Found.</b></h1>';
-            break;
-        case 1: //main page
-            html += '<h1><b>Informação</b></h1><ul>' +
-                    '<li><a href="localhost:4000/alunos">Alunos</a></li>'+
-                    '<li><a href="localhost:4000/cursos">Cursos</a></li>'+
-                    '<li><a href="localhost:4000/instrumentos">Instrumentos</a></li></ul>';
-            break;
-        case 2: //alunos
-            break;
-        case 3: //cursos
-            break;
-        case 4: //instrumentos
-            break;
-        default:
-            break;
+function generatePage(ismain){
+    let html = basehtml
+    if (ismain){
+        html += '<h1><b>Informação</b></h1><ul>' +
+                    '<li><a href="http://localhost:4000/alunos">Alunos</a></li>' +
+                    '<li><a href="http://localhost:4000/cursos">Cursos</a></li>' +
+                    '<li><a href="http://localhost:4000/instrumentos">Instrumentos</a></li></ul>';
     }
-
+    else{
+        html += '<h1><b>Page Not Found.</b></h1>';
+    }
     return html + '</body></html>'
 }
 
 http.createServer(function (req, res) {
 
     console.log(req.method + " " + req.url)
-    
     var path = url.parse(req.url, true).pathname
-    var pagetype = 0
 
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+    
+    // Main Page
     if (path=='/'){
-        pagetype = 1
+        res.write(generatePage(1))
     }
+
+    // Pagina Alunos
     else if (path.match(/\/alunos\/?$/)){
-        pagetype = 2
+        axios.get('http://localhost:3000/alunos').then(resp => {
+            let html = basehtml + '<h1><b>Alunos</b></h1><ul>'
+            resp.data.forEach(a => {
+                html += `<li>${a.id}, ${a.nome}, ${a.curso}, ${a.instrumento}</li>`
+            });
+            res.write(html + '</ul></body></html>')
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
-    else if (path.match(/\/cursos\/?$/)){
-        pagetype = 3
+
+    // Pagina Cursos
+    else if (path.match(/\/cursos\/?$/)){ 
+        axios.get('http://localhost:3000/cursos').then(resp => {
+            let html = basehtml + '<h1><b>Cursos</b></h1><ul>'
+            resp.data.forEach(a => {
+                html += `<li>${a.id}, ${a.designacao}, </li>`
+            });
+            res.write(html + '</ul></body></html>')
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
-    else if (path.match(/\/instrumentos\/?$/)){
-        pagetype = 4
+
+    // Pagina Instrumentos
+    else if (path.match(/\/instrumentos\/?$/)){ 
+        axios.get('http://localhost:3000/instrumentos').then(resp => {
+            let html = basehtml + '<h1><b>Instrumentos</b></h1><ul>'
+            resp.data.forEach(a => {
+                html += `<li>${a.id}, </li>`
+            });
+            res.write(html + '</ul></body></html>')
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    // Pagina de Erro
+    else{ 
+        res.write(generatePage(0))
     }
     
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-    res.write(generatePage(pagetype))
     res.end;
 
 }).listen(4000)
 
 console.log('Server listening on port 4000...')
+
+/*
+To do:
+ - access #text
+ - format tables
+ - update manifest
+*/
